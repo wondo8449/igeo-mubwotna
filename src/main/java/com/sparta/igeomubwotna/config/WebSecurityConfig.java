56efa8940ace4025c127e7c383630059f0f68f36
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-05 01:12:53 +0900

    ✨ update - #7 SecurityConfig에 회원가입 요청 모두 접근 허가할 수 있게 하는 코드 추가

--- src/main/java/com/sparta/igeomubwotna/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/igeomubwotna/config/WebSecurityConfig.java
@@ -1,11 +1,14 @@
 package com.sparta.igeomubwotna.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity // Spring Security 지원을 가능하게 함
@@ -14,5 +17,20 @@ public class WebSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // CSRF 설정
+        http.csrf((csrf) -> csrf.disable());
+
+        http.authorizeHttpRequests((authorizeHttpRequests) ->
+                authorizeHttpRequests
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
+                        .requestMatchers("/user/signup").permitAll() // 회원가입 요청 모두 접근 허가
+                        .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+        );
+
+        return http.build();
     }
 }
