abdec2dd4a06b1931a1b1af259441b540080614c
sujeongmoon <msjk9000@gmail.com>
2024-06-04 18:49:14 +0900

    🎉 add, ✨ update - Recpie 엔티티 추가 #1
    Signed-off-by: sujeongmoon <msjk9000@gmail.com>

--- src/main/java/com/sparta/igeomubwotna/entity/Recipe.java
+++ src/main/java/com/sparta/igeomubwotna/entity/Recipe.java
@@ -1,4 +1,33 @@
 package com.sparta.igeomubwotna.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "recipe")
+@NoArgsConstructor
 public class Recipe extends Timestamped{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	// @ManyToOne(fetch = FetchType.LAZY)
+	// @JoinColumn(name = "user_id", nullable = false)
+	// private User user;
+	@Column
+	private String title;
+	@Column
+	private String content;
+
 }
