b98f817f782174433b7111c7762fd0c710b89248
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-04 20:49:13 +0900

    ✨ update - #7 UserController에 signup 메서드 생성 후 성공 여부와 상태 코드 return하도록 구현

--- src/main/java/com/sparta/igeomubwotna/controller/UserController.java
+++ src/main/java/com/sparta/igeomubwotna/controller/UserController.java
@@ -1,4 +1,29 @@
 package com.sparta.igeomubwotna.controller;
 
+import com.sparta.igeomubwotna.dto.SignupRequestDto;
+import com.sparta.igeomubwotna.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+    @PostMapping("/user/signup")
+    public ResponseEntity<Map<String, Object>> signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "회원가입이 성공적으로 이루어졌습니다.");
+        response.put("status", HttpStatus.OK.value());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
--- src/main/java/com/sparta/igeomubwotna/service/UserService.java
+++ src/main/java/com/sparta/igeomubwotna/service/UserService.java
@@ -1,4 +1,10 @@
 package com.sparta.igeomubwotna.service;
 
+import com.sparta.igeomubwotna.dto.SignupRequestDto;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    public void signup(SignupRequestDto requestDto) {
+    }
 }
