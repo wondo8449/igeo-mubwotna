50c9079c85636f0d71e7cfc1a1e31e0e6aa0bfe3
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-05 00:57:24 +0900

    ✨ update - #7 회원가입시 상태 코드 및 성공 여부 return 할때 Response 사용해서 return 하도록 업데이트

--- src/main/java/com/sparta/igeomubwotna/controller/UserController.java
+++ src/main/java/com/sparta/igeomubwotna/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.sparta.igeomubwotna.controller;
 
+import com.sparta.igeomubwotna.dto.Response;
 import com.sparta.igeomubwotna.dto.SignupRequestDto;
 import com.sparta.igeomubwotna.service.UserService;
 import jakarta.validation.Valid;
@@ -10,22 +11,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
     @PostMapping("/user/signup")
-    public ResponseEntity<Map<String, Object>> signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+    public ResponseEntity<Response> signup(@RequestBody @Valid SignupRequestDto requestDto, BindingResult bindingResult) {
         // 유효성 검사 예외 처리
         List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-
-        Map<String, Object> response = new HashMap<>();
 
         // 유효성 검사에 오류가 있으면
         if(fieldErrors.size() > 0) {
@@ -34,19 +34,11 @@ public class UserController {
                 log.error(fieldError.getField() + " 필드: " + fieldError.getDefaultMessage());
             }
             // 오류 메시지와 상태 코드 반환
-            response.put("message", "회원가입에 실패하였습니다.");
-            response.put("status", HttpStatus.BAD_REQUEST.value());
-            response.put("errors", fieldErrors);
-            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+            Response response = new Response(HttpStatus.BAD_REQUEST.value(), "회원가입에 실패하였습니다.");
+            return ResponseEntity.badRequest().body(response);
         }
 
         // UserService의 signup 메서드에 데이터 넘겨 줌
-        userService.signup(requestDto);
-
-        // 성공 메시지와 상태 코드 반환
-        response.put("message", "회원가입이 성공적으로 이루어졌습니다.");
-        response.put("status", HttpStatus.OK.value());
-
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return userService.signup(requestDto);
     }
 }
