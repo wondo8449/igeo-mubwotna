95f568f1778b4b9842b8f68e86d5d18cada8e000
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-04 21:19:52 +0900

    🩹 fix - #7 User entity에서 유효성 검사 -> SignupRequestDto에서 하도록 변경

--- src/main/java/com/sparta/igeomubwotna/entity/User.java
+++ src/main/java/com/sparta/igeomubwotna/entity/User.java
@@ -1,14 +1,11 @@
 package com.sparta.igeomubwotna.entity;
 
-import java.time.LocalDateTime;
-
-import org.springframework.data.annotation.LastModifiedDate;
-
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -20,19 +17,14 @@ public class User extends Timestamped{
 	private Long id;
 
 	@Column(nullable = false, unique = true)
-	@Size(min = 10, max = 20, message = "사용자 ID는 최소 10글자 이상, 20글자 이하이어야 합니다.")
-	@Pattern(regexp = "^[a-zA-Z0-9]*$", message = "사용자 ID는 알파벳 대소문자, 숫자로만 구성되어야 합니다.")
 	private String userId;
 
-	@Size(min = 10, message = "password는 최소 10글자 이상이어야 합니다.")
-	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[_!#$%&'*+/=?`{|}~^.-])[a-zA-Z\\d]$", message = "password는 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자로만 구성되어야 합니다.")
 	@Column(nullable = false)
 	private String password;
 
 	@Column(nullable = false)
 	private String name;
 
-	@Pattern(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
 	@Column(nullable = false, unique = true)
 	private String email;
 
