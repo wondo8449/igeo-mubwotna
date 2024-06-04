418cb09809d5ff4b6671c1590c9821d93da7aac4
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-04 20:13:25 +0900

    🩹 fix - 쓰지 않는 import 제거

--- src/main/java/com/sparta/igeomubwotna/entity/User.java
+++ src/main/java/com/sparta/igeomubwotna/entity/User.java
@@ -1,8 +1,6 @@
 package com.sparta.igeomubwotna.entity;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.data.annotation.LastModifiedDate;
 
