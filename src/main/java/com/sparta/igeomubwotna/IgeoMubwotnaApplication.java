4ea24f236cb4a337fde7cd73144851b68bc64890
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-05 01:10:12 +0900

    ✨ update - Scheduling, JpaAuditiong import

--- src/main/java/com/sparta/igeomubwotna/IgeoMubwotnaApplication.java
+++ src/main/java/com/sparta/igeomubwotna/IgeoMubwotnaApplication.java
@@ -2,10 +2,13 @@ package com.sparta.igeomubwotna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling // Spring의 스케쥴링(미리 정한시간에 자동으로 작업 실행) 활성화
+@EnableJpaAuditing // 자동으로 생성, 수정 날짜 정보 추가
 @SpringBootApplication
 public class IgeoMubwotnaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(IgeoMubwotnaApplication.class, args);
     }
