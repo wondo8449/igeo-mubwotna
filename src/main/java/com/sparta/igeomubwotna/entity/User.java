87f9c9acf0aeb3ac5422bb102d2ca6822be3d024
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-05 00:54:19 +0900

    ✨ update - #7 회원 가입시 사용되는 생성자 구현

--- src/main/java/com/sparta/igeomubwotna/entity/User.java
+++ src/main/java/com/sparta/igeomubwotna/entity/User.java
@@ -11,42 +11,50 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @Table(name = "users")
-public class User extends Timestamped{
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+public class User extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(nullable = false, unique = true)
-	private String userId;
+    @Column(nullable = false, unique = true)
+    private String userId;
 
-	@Column(nullable = false)
-	private String password;
+    @Column(nullable = false)
+    private String password;
 
-	@Column(nullable = false)
-	private String name;
+    @Column(nullable = false)
+    private String name;
 
-	@Column(nullable = false, unique = true)
-	private String email;
+    @Column(nullable = false, unique = true)
+    private String email;
 
-	@Column(nullable = false)
-	private String description;
+    @Column(nullable = false)
+    private String description;
 
-	@Column(nullable = false)
-	@Enumerated(value = EnumType.STRING)
-	private UserStatusEnum status;
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserStatusEnum status;
 
-	@LastModifiedDate
-	@Column
-	@Temporal(TemporalType.TIMESTAMP)
-	private LocalDateTime statusModifiedAt;
-
-
-	// @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
-	// private List<Recipe> recipes = new ArrayList<>();
-	//
-	// @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
-	// private List<Comment> comments = new ArrayList<>();
+    @LastModifiedDate
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime statusModifiedAt;
 
 
-	// 생성자
+    // @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    // private List<Recipe> recipes = new ArrayList<>();
+    //
+    // @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    // private List<Comment> comments = new ArrayList<>();
+
+
+    // 생성자
+    public User(String userId, String password, String name, String email, String description) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.description = description;
+        this.status = UserStatusEnum.ACTIVE;
+    }
 }
