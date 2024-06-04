fd3ed91e9e675da7934c7aead47cf068d93dc171
eunsaem <sweeet0308@gmail.com>
2024-06-04 18:25:46 +0900

    🎉 add - #2 comment column 생성

--- src/main/java/com/sparta/igeomubwotna/entity/Comment.java
+++ src/main/java/com/sparta/igeomubwotna/entity/Comment.java
@@ -1,4 +1,37 @@
 package com.sparta.igeomubwotna.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "comment")
+@NoArgsConstructor
 public class Comment extends Timestamped{
+
+    /* Column */
+    //ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //레시피 ID
+    @Column(nullable = false)
+    private Long recipeId;
+
+    //작성자 ID
+    @Column(nullable = false)
+    private Long userId;
+
+    //내용
+    @Column(nullable = false)
+    private String content;
+
+    //좋아요 수
+    @Column(nullable = false)
+    private Long likeCount;
+
 }
