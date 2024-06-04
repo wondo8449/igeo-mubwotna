54979dc8e2c7145d677a4d875292f579ad1f82af
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-04 16:30:13 +0900

    🎉 add - 초기 파일 설정

---
+++ src/main/java/com/sparta/igeomubwotna/IgeoMubwotnaApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.igeomubwotna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgeoMubwotnaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgeoMubwotnaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/igeomubwotna/IgeoMubwotnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.igeomubwotna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgeoMubwotnaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
