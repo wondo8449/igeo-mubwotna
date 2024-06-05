a70004e2a3ccf454998dbe50956c8d3709f4856a
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-05 09:57:03 +0900

    🩹 fix -#7 원활한 협업을 위해 Security가 모든 접근 허가 하도록 수정

--- src/main/java/com/sparta/igeomubwotna/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/igeomubwotna/config/WebSecurityConfig.java
@@ -27,7 +27,8 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
                         .requestMatchers("/user/signup").permitAll() // 회원가입 요청 모두 접근 허가
-                        .anyRequest().authenticated() // 그 외 모든 요청 인증처리
+                        // TODO: 나중에 바꿔야 합니다.
+                        .anyRequest().permitAll() // 그 외 모든 접근도 모두 허가
         );
 
         return http.build();
