4768de1c2534d1d47b4751e1690b0f15a3619af9
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-05 10:10:15 +0900

    🩹 fix -#7 자세한 오류 설명을 위해 Response 클래스에 error 메시지를 담을 리스트 추가 후 생성자 구현

--- src/main/java/com/sparta/igeomubwotna/dto/Response.java
+++ src/main/java/com/sparta/igeomubwotna/dto/Response.java
@@ -1,16 +1,22 @@
 package com.sparta.igeomubwotna.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
+import java.util.List;
 @Getter
-@Setter
 public class Response {
-    private int status;
+    private int statusCode;
     private String message;
+    private List<String> errors;  // 필드 오류 메시지를 담을 리스트
 
-    public Response(int status, String message) {
-        this.status = status;
+    public Response(int statusCode, String message) {
+        this.statusCode = statusCode;
         this.message = message;
+    }
+
+    public Response(int statusCode, String message, List<String> errors) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.errors = errors;
     }
 }
