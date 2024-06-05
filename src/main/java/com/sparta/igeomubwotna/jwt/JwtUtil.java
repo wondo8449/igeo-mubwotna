64f0d0d8135c9e75ffff5d21d50ab1bed52c4b62
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-05 15:40:46 +0900

    🎉 add - #12 JwtUtil 생성

---
+++ src/main/java/com/sparta/igeomubwotna/jwt/JwtUtil.java
@@ -0,0 +1,13 @@
+package com.sparta.igeomubwotna.jwt;
+
+public class JwtUtil {
+    // JWT 생성
+
+    // 생성된 JWT를 Cookie에 저장
+
+    // Cookie에 들어있던 JWT 토큰을 Substring
+
+    // JWT 검증
+
+    // JWT에서 사용자 정보 가져오기
+}
