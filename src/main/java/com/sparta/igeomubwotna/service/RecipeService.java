c924a34820bd0c031830a137f737debe6cca6a84
sujeongmoon <msjk9000@gmail.com>
2024-06-05 14:45:13 +0900

    ✨ update - recipe 등록 기능 구현 #9
    Signed-off-by: sujeongmoon <msjk9000@gmail.com>

--- src/main/java/com/sparta/igeomubwotna/service/RecipeService.java
+++ src/main/java/com/sparta/igeomubwotna/service/RecipeService.java
@@ -1,4 +1,25 @@
 package com.sparta.igeomubwotna.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sparta.igeomubwotna.dto.RecipeRequestDto;
+import com.sparta.igeomubwotna.dto.RecipeResponseDto;
+import com.sparta.igeomubwotna.entity.Recipe;
+import com.sparta.igeomubwotna.entity.User;
+import com.sparta.igeomubwotna.repository.RecipeRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
 public class RecipeService {
+
+	private final RecipeRepository recipeRepository;
+
+	@Transactional
+	public RecipeResponseDto saveRecipe(RecipeRequestDto requestDto, User user) {
+		Recipe recipe = recipeRepository.save(new Recipe(requestDto, user));
+		return new RecipeResponseDto(recipe);
+	}
 }
