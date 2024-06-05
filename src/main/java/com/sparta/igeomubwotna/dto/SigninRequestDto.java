28fff9795743d7c393c1fe9c58b012bcf2b77da6
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-05 12:43:39 +0900

    🩹 fix - #12 SigninRequestDto getter,setter 추가, 유효성 검사 조건 추가

--- src/main/java/com/sparta/igeomubwotna/dto/SigninRequestDto.java
+++ src/main/java/com/sparta/igeomubwotna/dto/SigninRequestDto.java
@@ -1,6 +1,13 @@
 package com.sparta.igeomubwotna.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
 public class SigninRequestDto {
+    @NotBlank(message = "아이디는 공백일 수 없습니다.")
     private String userId;
+    @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
     private String password;
 }
