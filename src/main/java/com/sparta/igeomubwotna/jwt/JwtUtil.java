3eba89ccdda249df241e6d8549c1d07d6aaf4f54
이창형 <71262367+LeeChangHyeong@users.noreply.github.com>
2024-06-07 21:24:52 +0900

    🩹 fix - #31 Header key값 변경

--- src/main/java/com/sparta/igeomubwotna/jwt/JwtUtil.java
+++ src/main/java/com/sparta/igeomubwotna/jwt/JwtUtil.java
@@ -18,9 +18,9 @@ import java.util.Date;
 @Component
 public class JwtUtil {
     // AccessToken KEY 값 (이름)
-    public static final String ACCESS_HEADER = "ACCESS";
+    public static final String ACCESS_HEADER = "Authorization";
     // AccessToken KEY 값 (이름)
-    public static final String REFRESH_HEADER = "REFRESH";
+    public static final String REFRESH_HEADER = "X-Refresh-Token"; // X가 앞에 있으면 서버에서 발급한 키
 
     // 사용자 상태 값의 KEY (이름)
     public static final String AUTHORIZATION_KEY = "status";
