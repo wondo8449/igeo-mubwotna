c6f80388e9fb976bd535f37ebb1e434868d71bd7
sujeongmoon <msjk9000@gmail.com>
2024-06-10 01:13:51 +0900

    ✨ update - #38 기간별 조회 기능 추가
    Signed-off-by: sujeongmoon <msjk9000@gmail.com>

--- src/main/java/com/sparta/igeomubwotna/repository/RecipeRepository.java
+++ src/main/java/com/sparta/igeomubwotna/repository/RecipeRepository.java
@@ -1,9 +1,15 @@
 package com.sparta.igeomubwotna.repository;
 
+import java.time.LocalDateTime;
+
 import com.sparta.igeomubwotna.entity.Recipe;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+	Page<Recipe> findAllByCreatedAtBetween(Pageable pageable, LocalDateTime startDateTime, LocalDateTime endDateTime);
 }
