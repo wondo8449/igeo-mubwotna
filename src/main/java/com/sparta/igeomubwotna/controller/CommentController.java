eca7ad0d5accbaf02c43c1ef598bac7e7fe79c0d
박시현 <sihyun615@naver.com>
2024-06-10 23:59:48 +0900

    🩹 fix - 경로 앞에 /api 추가

--- src/main/java/com/sparta/igeomubwotna/controller/CommentController.java
+++ src/main/java/com/sparta/igeomubwotna/controller/CommentController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/recipe/{recipeId}/comment")
+@RequestMapping("/api/recipe/{recipeId}/comment")
 @RequiredArgsConstructor
 public class CommentController {
 
--- src/main/java/com/sparta/igeomubwotna/controller/UserController.java
+++ src/main/java/com/sparta/igeomubwotna/controller/UserController.java
@@ -12,34 +12,35 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/user/signup")
+    @PostMapping("/signup")
     public ResponseEntity<Response> signup(@RequestBody @Valid SignupRequestDto requestDto, BindingResult bindingResult) {
         // UserService의 signup 메서드에 데이터 넘겨 줌
         return userService.signup(requestDto, bindingResult);
     }
 
-    @GetMapping("/user/me")
+    @GetMapping("/me")
     public ResponseEntity<UserProfileDto> getCurrentUserProfile(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         // 인증 객체에서 사용자 정보를 추출
         UserProfileDto userProfile = userService.getUserProfile(userDetails.getUser().getId());
         return ResponseEntity.ok(userProfile);
     }
 
-    @PatchMapping("/user/me")
+    @PatchMapping("/me")
     public ResponseEntity<Response> updateUserProfile(@RequestBody @Valid UserUpdateRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         // 인증 객체에서 사용자 정보를 추출
         return userService.updateUserProfile(requestDto, userDetails.getUser().getId());
     }
 
-    @PostMapping("/user/logout")
+    @PostMapping("/logout")
     public ResponseEntity<Response> logout(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         return userService.logout(userDetails.getUser().getId());
     }
 
-    @PatchMapping("/user/withdraw")
+    @PatchMapping("/withdraw")
     public ResponseEntity<Response> withdrawUser(@RequestBody PasswordDto passwordDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         // 인증 객체에서 사용자 정보를 추출
         return userService.withdrawUser(passwordDto, userDetails.getUser().getId());
